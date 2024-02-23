a3e024b977f124415d2e69acb17bf39a0ad766a0
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2024-02-23 02:08:24 -0400

    reorder en A-estrella y descomentado la heuristica

--- global/A-estrella.cpp
+++ global/A-estrella.cpp
@@ -13,8 +13,8 @@ int a_estrella(state_t *init, unsigned (*heuristica) (state_t)){
     state_map_t *map = new_state_map();
     // Agrega el estado inicial a el state map y a la cola de prioridad 
     state_map_add(map, init, 0);
-    //q.Add(heuristic(*init), 0, *init);
-    q.Add(0, 0, *init);
+    q.Add(heuristica(*init), 0, *init);
+    // q.Add(0, 0, *init);
     int test = 5000;  // Esto es solo para ver los primeros estados y ver como avanza,
                       // Se puede quitar esto y la condicion "test != 0" del while
                       // una vez funcione.
@@ -32,10 +32,6 @@ int a_estrella(state_t *init, unsigned (*heuristica) (state_t)){
         estado = q.Top();
         q.Pop();
 
-        // Si el estado actual es el objetivo, retornamos el costo para alcanzarlo
-        if (is_goal(&estado)){
-            return d;
-        }
         // Pasamos al siguiente elemento del while si el mejor costo conocido
         // es menor que el costo actual
         const int *best_dist = state_map_get(map,&estado);
@@ -44,17 +40,24 @@ int a_estrella(state_t *init, unsigned (*heuristica) (state_t)){
         }
         // Actualizamos el costo del estado actual en el mapa
         state_map_add(map, &estado, d);
+
+        // Si el estado actual es el objetivo, retornamos el costo para alcanzarlo
+        if (is_goal(&estado)){
+            return d;
+        }
+
         // Iteramos por las posibles reglas que pueden ser aplicadas a el estado actual
         init_fwd_iter(&iter,&estado);     
         while(( ruleid = next_ruleid(&iter)) >= 0 ) {
             // Aplicamos la regla al estado actual para generar un estado hijo
             apply_fwd_rule( ruleid, &estado, &hijo );
-            // Si el estado hijo es alcanzable desde el estado objetivo, se calcula el costo para
-            // alcanzar el estado hijo
-            int hijo_d = d + get_fwd_rule_cost(ruleid);
-            int *ant_hijo_d = state_map_get(map, &hijo);
-
+            
             if(heuristica(hijo) < INT_MAX) {
+                // Si el estado hijo es alcanzable desde el estado objetivo, se calcula el costo para
+                // alcanzar el estado hijo
+                int hijo_d = d + get_fwd_rule_cost(ruleid);
+                int *ant_hijo_d = state_map_get(map, &hijo);
+
                 // Si el hijo no ha sido explorado antes, o si el nuevo costo es menor que el mejor conocido, 
                 // se actualiza el costo en el mapa y se agrega el hijo a la cola de prioridad
                 if((ant_hijo_d == NULL) || (*ant_hijo_d > hijo_d)) {
