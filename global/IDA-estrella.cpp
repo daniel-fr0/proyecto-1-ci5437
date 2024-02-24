69b29127239707afb317244e3d106fbec2a27fce
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2024-02-24 09:39:51 -0400

    IDA* con tiempo limite

--- global/IDA-estrella.cpp
+++ global/IDA-estrella.cpp
@@ -6,30 +6,53 @@
 #include <iostream>
 #include <string>
 #include <iterator>
+#include <atomic>
+#include <chrono>
+#include <future>
 
 using namespace std;
+
+atomic<bool> stop(false);
 
 pair<bool, int> f_bounded_dfs_visit(int bound, int g);
 
 state_t state;
 vector<int> path;
+int explored_distance, explored_nodes, total_nodes;
 
 int ida_estrella(state_t *init){
     state = *init;
     int bound = heuristica(state);
+    explored_distance = 0;
+    explored_nodes = 0;
+    total_nodes = 0;
 
-    while (true){
+    while (!stop){
         pair<bool, int> p = f_bounded_dfs_visit(bound, 0);
         if (p.first) return p.second;
         bound = p.second;
     }
+    return -1;
 }
 
 pair<bool, int> f_bounded_dfs_visit(int bound, int g){
+    if (g > explored_distance) {
+        cout << explored_nodes << " estados en distancia " << explored_distance << endl;
+        explored_distance = g;
+        explored_nodes = 0;
+    }
+
+    explored_nodes++;
+    total_nodes++;
+
     int h = heuristica(state);
     int f = g + h;
     if (f > bound) return make_pair(false, f);
-    if (h == 0) return make_pair(true, g);
+    if (is_goal(&state)) {
+        cout << explored_nodes << " estados en distancia " << explored_distance << endl;
+        cout << total_nodes << " estados totales" << endl;
+        return make_pair(true, g);
+    }
 
     int t = INT_MAX;
     ruleid_iterator_t iter;
@@ -40,7 +63,7 @@ pair<bool, int> f_bounded_dfs_visit(int bound, int g){
     hist = init_history;
 
     init_fwd_iter(&iter, &state);
-    while((ruleid = next_ruleid(&iter)) >= 0){
+    while((ruleid = next_ruleid(&iter)) >= 0 && !stop){
         if (!fwd_rule_valid_for_history(hist, ruleid)) continue;
         next_fwd_history(hist, ruleid);
 
@@ -66,6 +89,13 @@ int main(int argc, char const *argv[]) {
     if (argc < 2){
         cout<<"Uso: "<<argv[0]<<" <archivo con estado inicial>"<<endl;
         return 1;
+    }
+
+    int TIME_LIMIT = 300; // 5 minutos por defecto
+
+    // Tiempo limite opcional
+    if (argc > 2){
+        TIME_LIMIT = stoi(argv[2]);
     }
 
     // Abrimos el archivo con el estado inicial
@@ -95,11 +125,15 @@ int main(int argc, char const *argv[]) {
     cout << "Construyendo heuristica..." << endl;
     init_heuristica();
     cout << "Buscando camino..." << endl;
-    int costo = ida_estrella(&init);
 
-    if (costo == -1){
+    future<int> future = async(launch::async, ida_estrella, &init);
+
+    if (future.wait_for(chrono::seconds(3)) == future_status::timeout){
+        stop = true;
+        cout << "Tiempo de ejecucion excedido de " << TIME_LIMIT / 60 << " minutos" << endl;
         cout << "No se encontro camino" << endl;
     } else {
+        int costo = future.get();
         cout << "Costo del camino minimo encontrado: " << costo << endl;
 
         for (std::vector<int>::size_type i = 0; i < path.size(); i++){
