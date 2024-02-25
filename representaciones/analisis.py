639a17034c3db05ed557a38666f45984e7c712b8
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2024-02-24 23:27:21 -0400

    analisis.py es generico

--- representaciones/analisis.py
+++ representaciones/analisis.py
@@ -1,15 +1,23 @@
-import math
+import math, sys
 
 def obtener_valores(archivo):
 	with open(archivo) as f:
-		return [int(linea.split()[0]) for linea in f]
-
+		valores = []
+		# match line pattern "<int> states at distance <int>"
+		for linea in f:
+			if "states at distance" in linea:
+				valores.append(int(linea.split()[0]))
+	return valores
 
 def media_geometrica(valores):
 	producto = math.prod(valores)
 	return producto ** (1.0 / len(valores))
 
-valores = obtener_valores('resultado.txt')
+if len(sys.argv) != 2:
+	print("Uso: python analisis.py <archivo>")
+	sys.exit(1)
+
+valores = obtener_valores(sys.argv[1])
 ramificacion = [valores[i] / valores[i - 1] for i in range(1, len(valores))]
 print("ramificacion", ramificacion)
 print("promedio", media_geometrica(ramificacion))
